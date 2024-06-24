14aae74a037d15e12ec71ed0c6115c7effed75ff
Pham Thanh Thuan <thanhthuan009753@gmail.com>
2024-06-24 21:35:37 +0700

    init

---
+++ app/src/main/java/com/example/blogapp1/model/GalleryImages.java
@@ -0,0 +1,24 @@
+package com.example.blogapp1.model;
+
+import android.net.Uri;
+
+public class GalleryImages {
+
+    public Uri picUri;
+
+    public GalleryImages() {
+    }
+
+    public GalleryImages(Uri picUri) {
+        this.picUri = picUri;
+    }
+
+    public Uri getPicUri() {
+        return picUri;
+    }
+
+    public void setPicUri(Uri picUri) {
+        this.picUri = picUri;
+    }
+
+}
